6588f03f216881d66d69fccda461119349afa193
OldRepublic <34744616+OldRepublic@users.noreply.github.com>
2018-03-09 12:57:49 -0500

    Update Dungeon Crawler.js

---
+++ Dungeon Crawler.js
@@ -0,0 +1,8 @@
+var canvas =document.querySelector("canvas");
+//var surface = canvas.getContext("2d");
+var player = document.getElementById("Player");
+var playerX = 100;
+var playerY = 100;
+
+var uInt; // setinterval value
+var map; //background grid
