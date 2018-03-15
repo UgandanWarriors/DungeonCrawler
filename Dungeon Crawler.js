6e56c022138d6dc24a5bf9382b418770e45ef282
AsmelashT <asmelashtsehaye@hotmail.com>
2018-03-15 13:35:36 -0400

    changes for myself

--- Dungeon Crawler.js
+++ Dungeon Crawler.js
@@ -6,3 +6,28 @@ var playerY = 100;
 
 var uInt; // setinterval value
 var map; //background grid
+
+
+
+class playerCharacter {
+		
+		constructor (name, baseHP, baseMana, baseHeadArmour, baseChestArmour, baseLegArmour, baseGauntletArmour, baseMovement) {
+			
+			this.name = name;
+			this.baseHP = baseHP;
+			this.baseMana = baseMana;
+			this.baseHeadArmour = baseHeadArmour;
+			this.baseChestArmour = baseChestArmour;
+			this.baseLegArmour = baseLegArmour;
+			this.baseGauntletArmour = baseGauntletArmour;
+		}
+		
+		statCheck () {
+		
+			console.log(this.name + this.baseHP + this.baseMana + this.baseHeadArmour + this.baseChestArmour + this.baseLegArmour + this.baseGauntletArmour);
+		
+		}
+	}
+	
+	var cancer = new playerCharacter('Joe', 10, 10, 2, 5, 3, 2, 1);
+	cancer.statCheck();
